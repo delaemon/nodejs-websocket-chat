af2daf3f5e7429e601f98216b1e16194b1527ae2
delaemon <takeshi.music.5@gmail.com>
2016-01-15 00:57:40 +0900

    Refactor

--- server.js
+++ server.js
@@ -1,37 +1,34 @@
-var WebSocketServer = require('ws').Server
-    , http = require('http')
-    , express = require('express')
+var WebSocketServer = require("ws").Server
+    , http = require("http")
+    , express = require("express")
     , app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + "/"));
 var server = http.createServer(app);
 var wss = new WebSocketServer({server:server});
 
-var redis = require('redis')
-var sub = redis.createClient(6379, 'localhost');
-var pub = redis.createClient(6379, 'localhost');
-var log = redis.createClient(6379, 'localhost');
-var channel = 'channel1'
+var redis = require("redis")
+var sub = redis.createClient(6379, "localhost");
+var pub = redis.createClient(6379, "localhost");
+var log = redis.createClient(6379, "localhost");
+var channel = "channel1"
 
-wss.on('connection', function (ws) {
+wss.on("connection", function (ws) {
     sub.subscribe(channel);
-    log.lrange('log:' + channel, 0, -1, function(err, history){
-        console.log("(last) " + history.toString())
+    log.lrange("log:" + channel, 0, -1, function(err, history){
+        console.log("(history) count:" + history.length)
         ws.send(JSON.stringify(history));
     });
     console.log("(connection) count: " + wss.clients.length);
-    ws.on('close', function () {
+    ws.on("close", function () {
         console.log("(close) count: " + wss.clients.length);
     });
-    ws.on('message', function (message) {
+    ws.on("message", function (message) {
         var now = new Date().toString();
-        var msg = '[' + now + '] ' + message
-        console.log("(publish)" + msg);
-        log.lrange('log:' + channel, 0, -1, function(err, history){
-            history.push(msg)
-            console.log("(update) " + history.toString())
-            log.rpush('log:' + channel, history)
-        });
+        var msg = "[" + now + "] " + message
+        console.log("(log)")
+        log.rpush("log:" + channel, msg)
+        console.log("(publish) " + msg);
         pub.publish(channel, msg);
     });
 });
